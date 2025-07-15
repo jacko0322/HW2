d4ec47594e6aaff141fe655161393918dc4615f6
zhinbziyi <kangxinyou@gmail.com>
2025-07-15 21:16:17 +0800

    Update server.js

--- 貿協人培/server.js
+++ 貿協人培/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
+const newsApiRoutes = require('./news-api/server');  // 引入 news-api 路由
+const courseApiRoutes = require('./course-api/server');  // 引入 course-api 路由
+const fileApiRoutes = require('./file-api/server');  // 引入 file-api 路由
+const friendlyLinkApiRoutes = require('./friendly-link-api/server');  // 引入 friendly-link-api 路由
+const faqApiRoutes = require('./question-api/server');  // 引入 question-api 路由
+
 const app = express();
-const PORT = 3000;
+const port = 3004;  // 設定主伺服器端口
 
-// 引入模組的路由
-const newsApiRoutes = require('./news-api/server');
-const courseApiRoutes = require('./course-api/server');
-const fileApiRoutes = require('./file-api/server');
-const questionApiRoutes = require('./question-api/server');
+// 註冊各個模組的路由
+app.use('/api/news', newsApiRoutes);  // 註冊 news-api 路由
+app.use('/api/courses', courseApiRoutes);  // 註冊 course-api 路由
+app.use('/api/files', fileApiRoutes);  // 註冊 file-api 路由
+app.use('/api/friendly-links', friendlyLinkApiRoutes);  // 註冊 friendly-link-api 路由
+app.use('/api/faq', faqApiRoutes);  // 註冊 question-api 路由
 
-// 使用這些路由
-app.use('/api/news', newsApiRoutes); // 註冊 news-api 路由
-app.use('/api/courses', courseApiRoutes); // 註冊 course-api 路由
-app.use('/api/files', fileApiRoutes); // 註冊 file-api 路由
-app.use('/api/questions', questionApiRoutes); // 註冊 question-api 路由
+// 根路由，測試伺服器是否正常
+app.get('/', (req, res) => {
+  res.send('主伺服器正在運行！');
+});
 
 // 啟動伺服器
-app.listen(PORT, () => {
-    console.log(`伺服器運行在 http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`主伺服器正在運行於 http://localhost:${port}`);
 });
+
