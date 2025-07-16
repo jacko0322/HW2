37d3a8b6280b7f7b68eedefcb6ca811e62953396
zhinbziyi <kangxinyou@gmail.com>
2025-07-16 09:18:52 +0800

    Update server.js

--- 貿協人培/server.js
+++ 貿協人培/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cors = require('cors');  // 加入此行以引入 CORS 模組
+const cors = require('cors');  // 引入 cors 模組
 const newsApiRoutes = require('./news-api/server');  // 引入 news-api 路由
 const courseApiRoutes = require('./course-api/server');  // 引入 course-api 路由
 const fileApiRoutes = require('./file-api/server');  // 引入 file-api 路由
-const friendlyLinkApiRoutes = require('./friendly_link-api/server'); 
+const friendlyLinkApiRoutes = require('./friendly_link-api/server');  // 引入 friendly-link-api 路由
 const faqApiRoutes = require('./question-api/server');  // 引入 question-api 路由
 
 const app = express();
 const port = 3004;  // 設定主伺服器端口
 
-app.use(cors());
+app.use(cors());  // 使用 CORS
 
 // 註冊各個模組的路由
 app.use('/api/news', newsApiRoutes);  // 註冊 news-api 路由
